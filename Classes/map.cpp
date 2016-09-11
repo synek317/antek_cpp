898940040675ed3dfb56b431619db221aa6fbaf7
Marcin Sas-Szymanski <marcin.sas-szymanski@anixe.pl>
2016-09-11 23:27:12 +0200

    fog brute force

--- Classes/map.cpp
+++ Classes/map.cpp
@@ -17,6 +17,50 @@ void cleanup()
 	theLevelScene->removeChild(clip);
 }
 
+class FogSprite : public Sprite
+{
+public:
+	virtual void draw(Renderer *renderer, const Mat4 &transform, uint32_t flags) override
+	{
+		int oldEquation;
+		glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &oldEquation);
+		//glBlendEquation(GL_FUNC_SUBTRACT);
+		Sprite::draw(renderer, transform, flags);
+		glBlendEquation(oldEquation);
+	}
+
+	static FogSprite* createWithSpriteFrame(SpriteFrame *spriteFrame)
+	{
+		FogSprite *sprite = new (std::nothrow) FogSprite();
+		if (sprite && spriteFrame && sprite->initWithSpriteFrame(spriteFrame))
+		{
+			sprite->autorelease();
+			return sprite;
+		}
+		CC_SAFE_DELETE(sprite);
+		return nullptr;
+	}
+
+	static FogSprite* createWithSpriteFrameName(const std::string& spriteFrameName)
+	{
+		SpriteFrame *frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(spriteFrameName);
+
+#if COCOS2D_DEBUG > 0
+		char msg[256] = { 0 };
+		sprintf(msg, "Invalid spriteFrameName: %s", spriteFrameName.c_str());
+		CCASSERT(frame != nullptr, msg);
+#endif
+
+		return createWithSpriteFrame(frame);
+	}
+};
+
+int d = 0;
+int s = 8;
+FogSprite* sprite;
+FogSprite* sprite2;
+GLenum opts[] = { GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA };
+
 void create_clipping()
 {
 	auto levelSize = theLevelScene->getContentSize();
@@ -33,22 +77,66 @@ void create_clipping()
 		(levelSize.height - h) / 2
 		);
 
-	auto sprite = Sprite::createWithSpriteFrameName("fog");
+	sprite = FogSprite::createWithSpriteFrameName("fog");
 	sprite->setPosition(150, 150);
 
-	auto sprite2 = Sprite::createWithSpriteFrameName("fog");
+	sprite2 = FogSprite::createWithSpriteFrameName("fog");
 	sprite2->setPosition(250, 50);
 
+	auto ll = LayerColor::create(Color4B(0, 0, 0, 128), theMap->getContentSize().width, theMap->getContentSize().height);
 	
-	//RenderTexture* renderTexture = RenderTexture::create(levelSize.width, levelSize.height, Texture2D::PixelFormat::RGBA8888); // configure for clipping node
+	
+	auto eventListener = EventListenerKeyboard::create();
 
-	auto ll = LayerColor::create(Color4B(0, 0, 0, 255), theMap->getContentSize().width, theMap->getContentSize().height);
-	//renderTexture->beginWithClear(0.0f, 0.0f, 0.0f, 1.0f);
-	sprite->setBlendFunc({ GL_ZERO, GL_SRC_ALPHA});
-	sprite2->setBlendFunc({ GL_SRC_ALPHA, GL_ZERO });
-	ll->addChild(sprite);
-	ll->addChild(sprite2);
-	//renderTexture->end();
+	sprite->setBlendFunc({ opts[d], opts[s] });
+	sprite2->setBlendFunc({ opts[d], opts[s] });
+
+	eventListener->onKeyPressed = [](EventKeyboard::KeyCode keyCode, Event* event){
+
+		Vec2 loc = event->getCurrentTarget()->getPosition();
+		switch (keyCode){
+		case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+		case EventKeyboard::KeyCode::KEY_A:
+			d--;
+			if (d < 0)
+			{
+				d = 13;
+			}
+			break;
+		case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+		case EventKeyboard::KeyCode::KEY_D:
+			d++;
+			if (d > 13)
+			{
+				d = 0;
+			}
+			break;
+		case EventKeyboard::KeyCode::KEY_UP_ARROW:
+		case EventKeyboard::KeyCode::KEY_W:
+			s--;
+			if (s < 0)
+			{
+				s = 13;
+			}
+			break;
+		case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
+		case EventKeyboard::KeyCode::KEY_S:
+			s++;
+			if (s > 13)
+			{
+				s = 0;
+			}
+			break;
+		}
+
+		sprite->setBlendFunc({ opts[d], opts[s] });
+		sprite2->setBlendFunc({ opts[d], opts[s] });
+	};
+
+	theMap->getEventDispatcher()->addEventListenerWithSceneGraphPriority(eventListener, sprite);
+
+	clip->addChild(sprite);
+	clip->addChild(sprite2);
 	theMap->addChild(ll, 999);
 }
 
